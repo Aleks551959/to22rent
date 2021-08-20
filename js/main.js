6f0a599102b40a62d1285dedf1d18feb2bfe3139
Aleks551959 <alekseidudarenko@gmail.com>
2021-08-20 20:05:57 +0300

    header

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.header__slider').slick({
+
+    nextArrow: '<button class="slick-arrow slick-left"><img src="images/slider/arrow-left.svg" alt="prev arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-right"><img src="images/slider/arrow-right.svg" alt="prev arrow"></button>',
+
+
+
+});
