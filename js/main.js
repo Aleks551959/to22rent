d84e2591891363fd2d1c98719917b4e566834e70
Aleks551959 <alekseidudarenko@gmail.com>
2021-08-21 15:14:13 +0300

    reason

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $('.header__slider').slick({
 
     nextArrow: '<button class="slick-arrow slick-left"><img src="images/slider/arrow-left.svg" alt="prev arrow"></button>',
     prevArrow: '<button class="slick-arrow slick-right"><img src="images/slider/arrow-right.svg" alt="prev arrow"></button>',
-
+    fade: true,
 
 
 });
