b308b9f1c9724b3d7a63ee5b8b47beebbcf65626
Aleks551959 <alekseidudarenko@gmail.com>
2021-08-24 17:58:51 +0300

    to2rent end 2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $('.header__slider').slick({
     nextArrow: '<button class="slick-arrow slick-left"><img src="images/slider/arrow-left.svg" alt="prev arrow"></button>',
     prevArrow: '<button class="slick-arrow slick-right"><img src="images/slider/arrow-right.svg" alt="prev arrow"></button>',
     fade: true,
+    responsive: [{
+        breakpoint: 441,
+        settings: {
+            arrows: false,
+        }
+    }, ]
 
 
 });
